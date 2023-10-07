d8cc140ad522d4d0453b35329175cb0fb725314b
Martin Rudloff <m7rudloff@gmail.com>
2023-10-07 16:50:32 +0200

    Second frontend push

--- frontend/app.py
+++ frontend/app.py
@@ -5,13 +5,105 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
+import time
 
 
 
-st.write("Welcome")
+
+
+
+# Define the HTML and CSS styling for the fixed header
+header_html = """
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap');
+        .header-container {
+            background-color: #B22222;
+            padding: 35px; /* Increase padding to make it taller */
+            text-align: center;
+            width: 37%;
+            color: #FFFFFF; /* Set text color to white */
+            height: 150px; /* Adjust height to make it taller */
+            position: fixed; /* Make the header fixed at the top */
+            top: 0; /* Stick it to the top */
+            z-index: 1000; /* Adjust the z-index if needed */
+        }
+        .header-title {
+            font-family: 'Merriweather', serif;
+            font-size: 28px;
+            color: #FFFFFF; /* Set text color to white */
+        }
+        .header-subtitle {
+            font-family: 'Arial', sans-serif;
+            font-size: 18px;
+            color: #FFFFFF; /* Set text color to white */
+        }
+        .content {
+            margin-top: 170px; /* Adjust the margin to move content below the header */
+        }
+    </style>
+    <div class="header-container">
+        <h1 class="header-title">Emotion Speech Recognition</h1>
+        <h3 class="header-subtitle">Data Science Le Wagon Bootcamp Part-time</h3>
+    </div>
+"""
+
+# Render the HTML in Streamlit
+st.markdown(header_html, unsafe_allow_html=True)
+
+# Add content below the header
+st.write("This is your content below the header.")
+
+
+
+
+
+
+
+
+
+
+# Width of the original image (replace with the actual width)
+original_width = 800
+
+# Calculate half of the original width
+half_width = original_width // 3
+
+# Create a centered sidebar
+st.sidebar.image("lewagon.png", width=half_width)
+
+# List of dummy names
+names = ["Maryam Sadreddini", "Sina Naghizadeh", "Anna Snizhko", "Martin Jahr"]
+
+# Define the HTML and CSS styling for centering, making names bold, and increasing font size
+style_html = """
+    <style>
+        .sidebar-names {
+            text-align: center;
+            font-weight: bold;
+            font-size: 16px;
+        }
+    </style>
+"""
+
+# Render the HTML in Streamlit to apply CSS styling
+st.sidebar.markdown(style_html, unsafe_allow_html=True)
+
+# Display the names in the sidebar with the applied styling
+for name in names:
+    st.sidebar.markdown(f"<p class='sidebar-names'>{name}</p>", unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
 url = 'https://phoneme-service-wifbxua65a-ew.a.run.app'
 request = requests.get(url).json()["greeting"]
-st.write(request)
+#st.write(request)
 
 
 # Create a folder to store uploaded audio files (if it doesn't exist)
@@ -47,3 +139,25 @@ if audio_file is not None:
     st.pyplot(plt)
 else:
     st.warning("Please upload an audio file to view the spectrogram.")
+
+
+# Define the HTML and CSS styling for the footer
+footer_html = """
+    <style>
+        .footer-container {
+            background-color: #B22222;
+            padding: 10px;
+            text-align: center;
+            width: 37%;
+            color: #FFFFFF;
+            position: fixed;
+            bottom: 0;
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+    <div class="footer-container">batch-1283 Le Wagon</div>
+"""
+
+# Render the HTML in Streamlit
+st.markdown(footer_html, unsafe_allow_html=True)
